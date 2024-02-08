88590e75f7804e315c7e3c02193167a8f0adbbbf
Loliteyshn <lolita.culiuc19@gmail.com>
2024-02-08 17:55:33 +0200

    bookList-pagination

--- book-store/src/components/SearchAreaContainer.jsx
+++ book-store/src/components/SearchAreaContainer.jsx
@@ -1,22 +1,42 @@
 import { Component } from "react";
 import { connect } from "react-redux";
 import SearchArea from "./SearchArea";
-import { setSearchValue, setBookList } from "../store/searchReducer";
+import { setSearchValue, setBookList, setCurrentPage, setTotalBooksCount, setStartIndex, setPageSize } from "../store/searchReducer";
 import axios from "axios";
+import BookList from "./BookList";
 
 class SearchAreaContainer extends Component {
 
-    onHandleSearch() {
-        axios.get()
+    onHandleSearch = () => {
+        // console.log("tut: ", this.props.inputValue);
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.props.inputValue}&startIndex=${this.props.startIndex}&maxResults=${this.props.pageSize}`)
+            .then((response) => {
+                console.log(response);
+                this.props.setBookList(response.data.items);
+                this.props.setTotalBooksCount(response.data.totalItems)
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${this.props.inputValue}&startIndex=${pageNumber}&maxResults=${this.props.pageSize}`)
+            .then((response) => {
+                console.log(response);
+                this.props.setBookList(response.data.items);
+            });
     }
 
     render() {
         return <div>
             <SearchArea inputValue={this.props.inputValue}
                 setSearchValue={this.props.setSearchValue}
-                bookList={this.props.bookList}
                 setBookList={this.props.setBookList}
                 onHandleSearch={this.onHandleSearch} />
+            <BookList bookList={this.props.bookList}
+                totalBooksCount={this.props.totalBooksCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged} />
         </div>
     }
 
@@ -25,10 +45,17 @@ class SearchAreaContainer extends Component {
 let mapStateToProps = (state) => {
     return {
         inputValue: state.searchPage.inputValue,
-        bookList: state.searchPage.bookList
+        bookList: state.searchPage.bookList,
+        currentPage: state.searchPage.currentPage,
+        totalBooksCount: state.searchPage.totalBooksCount,
+        startIndex: state.searchPage.startIndex,
+        pageSize: state.searchPage.pageSize
     }
 }
 
 
 
-export default connect(mapStateToProps, { setSearchValue, setBookList })(SearchAreaContainer);
+export default connect(mapStateToProps, {
+    setSearchValue, setBookList, setCurrentPage, 
+    setTotalBooksCount, setStartIndex, setPageSize
+})(SearchAreaContainer);
--- book-store/src/store/searchReducer.js
+++ book-store/src/store/searchReducer.js
@@ -1,9 +1,17 @@
 const SET_SEARCH_VALUE = "SET_SEARCH_VALUE";
 const SET_BOOK_LIST = "SET_BOOK_LIST";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_BOOKS_COUNT = "SET_TOTAL_BOOKS_COUNT";
+const SET_START_INDEX = "SET_START_INDEX";
+const SET_PAGE_SIZE = "SET_PAGE_SIZE";
 
 let initialState = {
     inputValue: '',
-    bookList: null
+    bookList: [],
+    pageSize: 10,
+    totalBooksCount: 0,
+    startIndex: 0,
+    currentPage: 1
 }
 
 const searchReducer = (state = initialState, action) => {
@@ -18,6 +26,26 @@ const searchReducer = (state = initialState, action) => {
                 ...state,
                 bookList: action.bookList
             }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
+        case SET_TOTAL_BOOKS_COUNT:
+            return {
+                ...state,
+                totalBooksCount: action.totalBooksCount
+            }
+        case SET_START_INDEX:
+            return {
+                ...state,
+                startIndex: action.startIndex
+            }
+        case SET_PAGE_SIZE:
+            return {
+                ...state,
+                pageSize: action.pageSize
+            }
         default:
             return state;
     }
@@ -25,5 +53,9 @@ const searchReducer = (state = initialState, action) => {
 
 export const setSearchValue = (inputValue) => ({ type: SET_SEARCH_VALUE, inputValue });
 export const setBookList = (bookList) => ({ type: SET_BOOK_LIST, bookList });
+export const setCurrentPage = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage });
+export const setTotalBooksCount = (totalBooksCount) => ({ type: SET_TOTAL_BOOKS_COUNT, totalBooksCount });
+export const setStartIndex = (startIndex) => ({ type: SET_START_INDEX, startIndex });
+export const setPageSize = (pageSize) => ({ type: SET_PAGE_SIZE, pageSize });
 
 export default searchReducer;
